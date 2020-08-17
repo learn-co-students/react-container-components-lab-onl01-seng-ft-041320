eabb84efe3752d1a84308e8c276b22488c86ed09
rschlosser11 <rosalinschlosser@gmail.com>
2020-08-17 14:11:00 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReveiws = (props) => {
+    return (
+        <ul className='review-list'>
+            {props.reviews.map(review => {return <li data-title={review.display_title} className='review'>{review.display_title}</li>})}
+        </ul>
+    )
+}
+
+export default MovieReveiws;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,43 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    state = {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    findReviews = () => {
+        fetch(`${URL}&query=${this.state.searchTerm}`).then(resp => resp.json()).then(obj => this.setState({reviews: obj.results}), () => console.log(this.state.reviews))
+    }
+
+    simulate = (e) => {
+        e.preventDefault();
+        let query = e.target.value;
+        this.setState({
+            searchTerm: query
+        })
+        this.findReviews()
+    };
+
+    handleChange = (e) => {
+        let search = e.target.value;
+        this.setState({
+            searchTerm: search
+        })
+    }
+
+    render () {
+        return (
+            <div className='searchable-movie-reviews'>
+                <form onSubmit={this.simulate} >
+                    <input onChange={this.handleChange} type='text' name='search' value={this.state.searchTerm} />
+                    <input type='submit' value='Search' />
+                </form>
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+            
+        )
+    }
+}
+export default SearchableMovieReviewsContainer
