561938e10e36d6316af28ef9b7186c7ea241ff16
Franklyn Rodriguez <franklynr92@gmail.com>
2020-08-24 12:27:25 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -7,12 +7,12 @@ const MovieReviews = props => {
     return (
         <div className="review-list">
             <ul>
-            {props.map(review => <li key={review.link.url}><a href={review.link.url}>{review.link.type} {review.link.suggested_link_text}</a></li>)}
+            {console.log(props)}
             </ul>     
         </div>
     )
 }
 // {props.reviews.map((review) => console.log(review))}
-// 
+// {props.map(review => <li key={review.link.url}><a href={review.link.url}>{review.link.type} {review.link.suggested_link_text}</a></li>)}
 
 export default MovieReviews;
